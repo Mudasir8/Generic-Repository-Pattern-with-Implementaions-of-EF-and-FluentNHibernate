7023fd0eae60e01fe22a033f2f05c653a4ce07f1
mudasiryounas12 <mudasiryounas12@hotmail.com>
2018-08-22 23:16:32 +0300

    AngularSPA

---
+++ AngularSPA/Controllers/StudentController.cs
@@ -0,0 +1,68 @@
+﻿using Models;
+using Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularSPA.Controllers
+{
+    public class StudentController : Controller
+    {
+        // DI is provided by IoCBuilder class using AutoFac for NH implementaion of IUnitOfWork in "Project.Repositories.Implementations.NH" using AutoFac DI
+        IUnitOfWork _unitOfWork;
+
+        public StudentController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public ActionResult Index()
+        {
+           // IEnumerable<Student> students = _unitOfWork.Students.GetAllRecord();
+            return View();
+        }
+
+
+        public ActionResult Details(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(_unitOfWork.Students.GetByID(id.Value));
+        }
+
+        public ActionResult AddNew()
+        {
+            return View();
+        }
+
+        public string GetStudentsList()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(_unitOfWork.Students.GetAllRecord().ToList());
+        }
+
+        public bool AddNewStudent(string firtName, string lastName)
+        {
+            try
+            {
+                Student st = new Student
+                {
+                    FirstName = firtName,
+                    LastName = lastName
+                };
+                _unitOfWork.Students.AddNewRecord(st);
+                _unitOfWork.CommitChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+        }
+    }
+}
---
+++ AngularSPA/Scripts/Angualr/app.js
@@ -0,0 +1,12 @@
+﻿
+var myApp = angular.module('myApp', [])
+
+myApp.controller('myController', function MyController($scope, $http) {
+
+    // get list from Student/GetStudentsList
+    $http.get("/Student/GetStudentsList")
+        .then(function (response) {
+            $scope.studentList = response.data;
+        });
+
+}); // /controller
